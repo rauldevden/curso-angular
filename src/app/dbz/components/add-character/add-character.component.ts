a731fe1584c4c1c4b9d26295c808356543664325
Raul Denis Chetan <raul.denis.contractor@bbva.com>
2024-07-08 11:18:18 +0200

    aplicacion DBZ con borrar y anadir

--- src/app/dbz/components/add-character/add-character.component.ts
+++ src/app/dbz/components/add-character/add-character.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Character } from './../../interfaces/character.interface';
 
 @Component({
-  selector: 'app-add-character',
+  selector: 'dbz-add-character',
   templateUrl: './add-character.component.html',
   styleUrl: './add-character.component.css'
 })
 export class AddCharacterComponent {
+
+  @Output()
+  public emitNewCharacter: EventEmitter<Character> = new EventEmitter();
+
+  public character: Character = {
+    name: '',
+    power: 0,
+  }
+
+  cleanForm(){
+    this.character.name = '';
+    this.character.power = 0;
+  }
+
+  onNewCharacter(){
+    this.character.name.length !== 0
+      ? this.emitNewCharacter.emit({...this.character})
+      : ''
+
+    this.cleanForm();
+  }
 
 }
--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, EventEmitter, Output} from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
-  selector: 'app-dbz-list',
+  selector: 'dbz-list',
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+
+  @Output()
+  public emitDeleteCharacter: EventEmitter<number> = new EventEmitter();
+
+  @Input()
+  public characterList: Character[] =[{
+    name:'Trunks',
+    power: 10
+  }]
+
+  onDeleteCharacter(index: number):void{
+    this.emitDeleteCharacter.emit(index);
+  }
 
 }
