ca20e8a79376c9ff8a620444f170acf4c3da8d40
Raul Denis Chetan <raul.denis.contractor@bbva.com>
2024-07-16 12:16:29 +0200

    docs creado

--- src/app/dbz/services/dbz.service.ts
+++ src/app/dbz/services/dbz.service.ts
@@ -2,9 +2,6 @@ import { Injectable } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
 import { v4 as uuid } from 'uuid';
 
-console.log(uuid());
-
-
 @Injectable({providedIn: 'root'})
 export class DbzService {
 
